5d08d30b24b2cc0122d084cee058cdab7704e649
farizmursyidan <farizmursyidan@gmail.com>
2026-03-07 14:47:04 +0700

    upgrade backend to connect to postgres

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+import psycopg2
 
 app = Flask(__name__)
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST", "postgres-service"),
+        database=os.getenv("DB_NAME", "mydb"),
+        user=os.getenv("DB_USER", "myuser"),
+        password=os.getenv("DB_PASSWORD", "mypassword")
+    )
 
 @app.route("/")
 def home():
     return {"message": "Hello from Kubernetes backend"}
+
+@app.route("/db-check")
+def db_check():
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        version = cur.fetchone()
+        cur.close()
+        conn.close()
+        return jsonify({"status": "success", "db_version": version[0]})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
